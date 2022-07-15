43877401498038bc6e58884af02f13014cbd6149
nabekitikun <nabekitikun6@gmail.com>
2022-07-15 10:32:23 +0000

    first commit

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @component('components.header')
+        @endcomponent
+        <div class="container pb-5" style="padding-top: 5rem;">
+             @component('components.flash')
+             @endcomponent
+            @yield('content')
+        </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
